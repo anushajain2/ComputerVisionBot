304b5a5e3092bd11ea873015722ea016cd53fc33
Anusha <an@dhcp-hol-312.eduroam.cornell.edu>
2017-04-16 17:28:09 -0400

    Initial Commit

---
+++ PictureBot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PictureBot
+//
+//  Created by Anusha on 4/16/17.
+//  Copyright © 2017 AnushaJain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
